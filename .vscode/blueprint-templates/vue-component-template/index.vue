f5d9c4addc8cb453ddf4a96d29709dfee21d842d
kanboo <wang.kanboo@gmail.com>
2018-08-13 11:04:23 +0800

    add vscode setting

---
+++ .vscode/blueprint-templates/vue-component-template/index.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: '{{ camelCase name }}',
+  components: {},
+  data() {
+    return {
+      title: 'Vue - Porject'
+    }
+  }
+}
+</script>
+
+<template src="./template.html"></template>
+<style lang="scss" src="./style.scss" scoped></style>
